348a895a9f01a41882edfbf0d40661fa5343ff09
prathamdupare <prathamdupare@proton.me>
2023-08-18 16:03:10 +0530

    Updated Frontend and backend

---
+++ script.js
@@ -0,0 +1,61 @@
+let gameover = false;
+
+const board = Array(9).fill('');
+
+const gameboard = document.querySelector('.gameboard');
+let turn = "X";
+
+const changeTurn = () => {
+    return turn === "X" ? "O" : "X";
+};
+
+const checkWin = () => {
+    const boxtexts = document.getElementsByClassName('square-text');
+    const wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    wins.forEach((e) => {
+        if (
+            boxtexts[e[0]].innerText === boxtexts[e[1]].innerText &&
+            boxtexts[e[2]].innerText === boxtexts[e[1]].innerText &&
+            boxtexts[e[0]].innerText !== ''
+        ) {
+            document.querySelector(".turn-for").innerText = `Player ${boxtexts[e[0]].innerText} wins!`;
+            gameover = true;
+        }
+    });
+};
+
+const createSquare = (index) => {
+    const square = document.createElement('div');
+    square.className = 'square';
+    gameboard.appendChild(square);
+
+    const squareText = document.createElement('div');
+    squareText.className = 'square-text';
+    square.appendChild(squareText);
+
+    square.addEventListener("click", () => {
+        if (squareText.innerText === '' && !gameover) {
+            squareText.innerText = turn;
+            turn = changeTurn();
+            checkWin();
+            if (!gameover) {
+                document.querySelector(".turn-for").innerText = `Turn for ${turn}`;
+            }
+        }
+    });
+};
+
+board.forEach((item, index) => {
+    createSquare(index);
+});
+
